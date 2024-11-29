f85b6f00b9cb945382ede2c24046c7784ca5e636
Michiel R <michielrodgers@gmail.com>
2024-11-29 13:57:54 +0100

    Opdracht_3

--- week2/contact.php
+++ week2/contact.php
@@ -10,6 +10,17 @@
     <?php
         include 'navbar.php';
     ?>
+        <form></form>
+    <label for="Voornaam">Naam</label >
+    <input type="text" name="Naam" pattern="[A-Za-z]" id="naam"><br><br>
+    
+    <label>E-mail:</label>
+    <input type="email" name="email" id="email" required><br><br>
+
+    <label for="Bericht">Bericht</label>
+    <input type="text" name="Bericht" id="bericht"><br><br>
+
+    <input type="submit" value="Submit">
     <?php
         include 'footer.php';
     ?>
